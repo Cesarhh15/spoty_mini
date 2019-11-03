813a87c4e16ad59b01d107f37abccc65b405bced
Cesar Huerta <cesarhh15@gmail.com>
2019-11-02 19:02:52 -0600

    spoty_mini

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//cargar rutas
+var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
+var album_routes = require('./routes/album');
+var song_routes = require('./routes/song');
+
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.json());
+
+// cabeceras http
+
+// rutas base
+app.use('/api',user_routes);
+app.use('/api',artist_routes);
+app.use('/api',album_routes);
+app.use('/api',song_routes);
+
+module.exports = app;
+
