56fb31a1e5d231df33641519f2a33a2b4abfcc9d
Cesar Huerta <cesarhh15@gmail.com>
2019-11-03 12:35:34 -0600

    Configuraciones finales backend

--- app.js
+++ app.js
@@ -11,16 +11,25 @@ var artist_routes = require('./routes/artist');
 var album_routes = require('./routes/album');
 var song_routes = require('./routes/song');
 
-app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // cabeceras http
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
 // rutas base
-app.use('/api',user_routes);
-app.use('/api',artist_routes);
-app.use('/api',album_routes);
-app.use('/api',song_routes);
+app.use('/api', user_routes);
+app.use('/api', artist_routes);
+app.use('/api', album_routes);
+app.use('/api', song_routes);
 
 module.exports = app;
 
--- controllers/song.js
+++ controllers/song.js
@@ -50,7 +50,121 @@ function saveSong(req, res) {
 
 }
 
+function getSongs(req, res) {
+    var albumId = req.params.album;
+    var find = "";
+    if (!albumId) {
+        find = Song.find({}).sort('number');
+    } else {
+        find = Song.find({ album: albumId }).sort('number');
+    }
+
+    find.populate({
+        path: 'album',
+        populate: ({
+            path: 'artist',
+            model: 'Artist'
+        })
+    }).exec(function (err, songs) {
+        if (err) {
+            res.status(500).send({ message: 'Error en la petición' });
+        } else {
+            if (!songs) {
+                res.status(404).send({ message: 'No hay canciones' });
+            } else {
+                res.status(200).send({ songs });
+            }
+        }
+    });
+}
+
+function updateSong(req, res) {
+    var songId = req.params.id;
+    var update = req.body;
+
+    Song.findByIdAndUpdate(songId, update, (err, songUpdated) => {
+        if (err) {
+            res.status(500).send({ message: 'Error en el servidor al actualizar la canción' });
+        } else {
+            if (!songUpdated) {
+                res.status(404).send({ message: 'No se ha actualizado la canción' });
+            } else {
+                res.status(200).send({ song: songUpdated });
+            }
+        }
+    });
+
+}
+
+function deleteSong(req, res) {
+    var songId = req.params.id;
+
+    Song.findByIdAndRemove(songId, (err, songRemoved) => {
+        if (err) {
+            res.status(500).send({ message: 'Error en el servidor al eliminar cancion' });
+        } else {
+            if (!songRemoved) {
+                res.status(404).send({ message: 'No se ha eliminado la canción' });
+            } else {
+                res.status(200).send({ song: songRemoved });
+            }
+        }
+    });
+
+}
+
+function uploadFile(req, res) {
+    var songId = req.params.id;
+    var file_name = "No ha subido...";
+
+    if (req.files) {
+        var file_path = req.files.file.path;
+        var file_split = file_path.split('/');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+
+        if (file_ext.toLowerCase() == 'mp3' || file_ext.toLowerCase() == 'ogg') {
+            Song.findByIdAndUpdate(songId, { file: file_name }, (err, songUpdated) => {
+                if (err) {
+                    res.status(500).send({ message: 'Error al actualizar canción' });
+                } else {
+                    if (!songUpdated) {
+                        res.status(404).send({ message: 'No se ha podido actualizar el canción' });
+                    } else {
+                        res.status(200).send({ song: songUpdated });
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({ message: 'Extensión del archivo no valida' });
+        }
+
+    } else {
+        res.status(200).send({ message: 'No has subido ningun archivo...' });
+    }
+}
+
+function getSongFile(req, res) {
+    var songFile = req.params.songFile;
+    var path_file = './uploads/songs/' + songFile;
+    fs.exists(path_file, function (exists) {
+        if (exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(200).send({ message: 'No existe el archivo...' });
+        }
+    });
+}
+
 module.exports = {
     getSong,
-    saveSong
+    saveSong,
+    getSongs,
+    updateSong,
+    deleteSong,
+    uploadFile,
+    getSongFile
 };
